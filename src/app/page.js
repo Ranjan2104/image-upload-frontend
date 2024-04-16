a232e92764464d4374e32f4d6c213eee7c185f2d
Ranjan2104 <amreshm@spanidea.com>
2024-04-16 13:44:44 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,74 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
+import axios from "axios";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 export default function Home() {
+  const cld = new Cloudinary({ cloud: { cloudName: "dltsmhfit" } });
+  const [show, isShow] = useState(false);
+  const [isDisable, setIsDisable] = useState(true);
+  const [url, setURL] = useState("");
+  const [imgData, setImgData] = useState();
+  const handleUpload = async () => {
+    setIsDisable(false);
+    const data = new FormData();
+    data.append("file", imgData);
+    data.append("upload_preset", "ml_default");
+    data.append("cloud_name", "dltsmhfit");
+    try {
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/dltsmhfit/image/upload",
+        data
+      );
+      setURL(res.data.secure_url);
+      setIsDisable(true);
+      isShow(true);
+    } catch (error) {
+      setIsDisable(true);
+      console.log("error", error.message);
+    }
+  };
+
+  const hanldeCopy = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        alert("Copied");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1>Upload your QR Code</h1>
+      <div className="form">
+        <input type="file" onChange={(e) => setImgData(e.target.files[0])} />
+        {isDisable ? (
+          <button className="btn" onClick={handleUpload}>
+            Upload
+          </button>
+        ) : (
+          <button className="btn" disabled>
+            Please wait...
+          </button>
+        )}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {show && (
+        <div className="copy">
+          <h3>Your URL is {url}</h3>
+          <button className="btn-copy" onClick={hanldeCopy}>
+            Copy
+          </button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   );
 }
